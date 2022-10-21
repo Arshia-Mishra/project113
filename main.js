5e95385204c92647f860ad2333628bb7fd5e7dda
Arshia-Mishra <80540337+Arshia-Mishra@users.noreply.github.com>
2022-10-21 19:42:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload() {
+
+}
+
+
+function setup() {
+    <center>canvas = createCanvas(300,300);</center>
+
+}
+
+
+function draw(){
+
+}
+
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+
+
